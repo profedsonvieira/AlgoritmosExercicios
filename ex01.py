98959ee096b912b5029d06ae6ecaa0ab5907086b
profedsonvieira <edson.vieira@faculdadeimpacta.com.br>
2026-08-18 10:48:22 -0300

    Initial Commit

---
+++ ex01.py
@@ -0,0 +1,20 @@
+nome = "Python"
+print(nome)
+#Python
+2 + 3 * 4
+#14
+
+mensagem = "Olá,Python!"
+print(mensagem)
+#Olá, Python!
+matematica = 2 + 3 * 4
+#14
+print(matematica)
+texto= "2 + 3 * 4"
+print(texto)
+
+expressao = texto + " = " + str(matematica)
+print(expressao)
+
+expressao1 = f"{texto} = {matematica}"
+print(expressao1)
